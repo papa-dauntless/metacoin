01f9814ba4e553e6bd2356d9b2d36227437e3058
Papa Dauntless <55884881+papa-dauntless@users.noreply.github.com>
2025-03-08 15:28:12 +0700

    Papa Dauntless

--- metadata.js
+++ metadata.js
@@ -1,16 +1,8 @@
 {
   "name": "Papa Dauntless",
-  "symbol": "DAUNT",
   "image": "https://raw.githubusercontent.com/papa-dauntless/metacoin/refs/heads/main/coin.png",
-  "description": "The Official Papa Dauntless Token",
-  "profile":{
-    "address":"DauntVgxZZ8PaCWyX3jScxfWHc3Q71krKRCqoK9wozoX",
-    "supply": 161610000,
-    "mintAuthority": "BossuC3iMRSkumnbTfVZzCuBAUrt9WK7ArUmm56FCUpp"
-  },
-  "creator": {
-    "name": "Papa Dauntless",
-    "site": "",
-    "telegram": "https://t.me/PapaDauntless"
-  }
+  "symbol": "DAUNT",
+  "description": "The Official Papa Dauntless Token.",
+  "telegram": "https://t.me/PapaDauntless",
+  "website": ""
 }
