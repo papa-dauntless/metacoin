89767a4b65ecfcf0b2874dd8a226c4a32484482e
Papa Dauntless <55884881+papa-dauntless@users.noreply.github.com>
2025-03-08 13:21:23 +0700

    Papa Dauntless

---
+++ metadata.js
@@ -0,0 +1,11 @@
+{
+  "name": "Papa Dauntless",
+  "symbol": "DAUNT",
+  "image": "https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I",
+  "description": "The Official Papa Dauntless Token",
+  "creator": {
+    "name": "NativeLabs",
+    "site": "",
+    "telegram": "https://t.me/PapaDauntless"
+  }
+}
